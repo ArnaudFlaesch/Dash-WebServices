d72530f4c30817708f1f59ca3806b03d372f1c2c
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2020-07-06 22:01:47 -0400

    Fix test and adding dataset

--- src/test/kotlin/com/dash/repository/WidgetRepositoryTests.kt
+++ src/test/kotlin/com/dash/repository/WidgetRepositoryTests.kt
@@ -11,12 +11,16 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
 @SpringBootTest
+@TabDataset
 @ExtendWith(SpringExtension::class)
-class WidgetRepositoryTests(@Autowired val widgetRepository: WidgetRepository) {
+class WidgetRepositoryTests() {
+
+    @Autowired
+    private lateinit var widgetRepository: WidgetRepository
 
     @Test
     fun `basic entity checks`() {
-        val p = Widget(1,2, null, null, null)
+        val p = Widget(1,2, null, null, Tab(1, null, null))
         widgetRepository.save(p)
         assertThat(widgetRepository.findAll()).hasSize(1)
     }
