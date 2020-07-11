d770c52a8a8ab1affad69fd5a86a5bb103d80dca
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2020-07-11 16:21:25 -0400

    Changed docker script

--- docker_deploy.sh
+++ docker_deploy.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build -t dashservices .
-docker tag dashservices arnaudf93/dashwebservices:latest
 docker push arnaudf93/dashwebservices:latest
