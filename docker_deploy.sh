22ba2d0cc0953ad80eb0bb90faa7b2211a0c1087
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2020-07-11 16:38:07 -0400

    Fix docker deploy

--- docker_deploy.sh
+++ docker_deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker build -t dashwebservices .
+docker tag dashwebservices arnaudf93/dashwebservices:latest
 docker push arnaudf93/dashwebservices:latest
