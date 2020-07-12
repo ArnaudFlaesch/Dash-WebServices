c732d38979aa138e4819ed2bf56d0507dd568b31
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2020-07-11 21:31:23 -0400

    Test dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
 FROM openjdk:13-jdk-alpine
 EXPOSE 8080
-ADD build/libs/dash-webservices-0.0.1.jar dash-webservices.jar
+ADD build/libs/dash-webservices-0.0.1.jar app/dash-webservices.jar
 CMD ["java", "-jar", "dash-webservices.jar"]
--- docker_deploy.sh
+++ docker_deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build -t dashwebservices .
+mkdir app
+docker build -t dashwebservices app
 docker tag dashwebservices arnaudf93/dashwebservices:latest
 docker push arnaudf93/dashwebservices:latest
