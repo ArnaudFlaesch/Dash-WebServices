4d567ced94b04acfa8a5f8eaddc289266f932410
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2021-04-03 17:40:03 -0400

    revert

--- src/main/kotlin/com/dash/entity/Tab.kt
+++ src/main/kotlin/com/dash/entity/Tab.kt
@@ -13,7 +13,7 @@ data class Tab(
     @SequenceGenerator(name = "tab-seq-gen", sequenceName = "tab_id_seq", initialValue = 1, allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "tab-seq-gen")
     @Column(name = "id", unique = true, nullable = false)
-    val id: Int,
+    var id: Int? = null,
 
     var label: String? = null,
 
