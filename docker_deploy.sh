6c9371badf8b711a7db015aa128f9bfba24d04a1
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2021-05-14 14:01:06 +0200

    Remove Travis CI

--- docker_deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
-docker build -t dashwebservices .
-docker tag dashwebservices arnaudf93/dashwebservices:latest
-docker push arnaudf93/dashwebservices:latest
