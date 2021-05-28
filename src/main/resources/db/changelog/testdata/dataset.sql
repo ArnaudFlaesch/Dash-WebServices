f3cdf393294898e29a36e21a6d72115fce48c466
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2021-05-28 21:27:49 +0200

    fix

--- src/main/resources/db/changelog/testdata/dataset.sql
+++ src/main/resources/db/changelog/testdata/dataset.sql
@@ -1,2 +1,2 @@
-INSERT INTO users(username, password, email, role_id) VALUES('usertest', '$2a$10$B0iEc.x8ZfcE7Eg7LT60fe368NRsqQ3K5t2Jf99umqiojH39CUcqi', 'user@email.com', 1);
-INSERT INTO users(username, password, email, role_id) VALUES('admintest', '$2a$10$B0iEc.x8ZfcE7Eg7LT60fe368NRsqQ3K5t2Jf99umqiojH39CUcqi', 'admin@email.com', 2);
+INSERT INTO users(username, password, email, role_id) VALUES('usertest', '$2a$10$7s8i0LOTm6CRvONAMlJ9POjtUxXjvPyQXwgQfrWTzbi2lvRjNRLYK', 'user@email.com', 1);
+INSERT INTO users(username, password, email, role_id) VALUES('admintest', '$2a$10$BKm6btRYxNdgyypNRvlwvOwDGcfL9Y/YSS5Cd25.S5VkIqV41iXmi', 'admin@email.com', 2);
