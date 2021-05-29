edac583392bfd36834ca03847f9a094935fb11bd
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2021-05-29 14:32:22 +0200

    format

--- src/main/kotlin/com/dash/controller/AuthController.kt
+++ src/main/kotlin/com/dash/controller/AuthController.kt
@@ -1,7 +1,5 @@
 package com.dash.controller
 
-import com.dash.repository.RoleRepository
-import com.dash.repository.UserRepository
 import com.dash.security.JwtUtils
 import com.dash.security.UserDetailsImpl
 import com.dash.security.payload.LoginRequest
@@ -11,7 +9,6 @@ import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.context.SecurityContextHolder
-import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.web.bind.annotation.*
 import java.util.stream.Collectors
 import javax.validation.Valid
@@ -22,15 +19,6 @@ import javax.validation.Valid
 class AuthController {
     @Autowired
     private lateinit var authenticationManager: AuthenticationManager
-
-    @Autowired
-    private lateinit var userRepository: UserRepository
-
-    @Autowired
-    private lateinit var roleRepository: RoleRepository
-
-    @Autowired
-    private lateinit var encoder: PasswordEncoder
 
     @Autowired
     private lateinit var jwtUtils: JwtUtils
