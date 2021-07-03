cefda4a355c4cb6856ee3d9a722415f7040c3fc8
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2021-07-03 20:57:56 +0200

    Fix

--- src/main/kotlin/com/dash/controller/ProxyController.kt
+++ src/main/kotlin/com/dash/controller/ProxyController.kt
@@ -1,6 +1,5 @@
 package com.dash.controller
 
-import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.*
 import java.net.URI
 import java.net.http.HttpClient
@@ -11,8 +10,6 @@ import java.net.http.HttpResponse
 @CrossOrigin(origins = ["*"])
 @RequestMapping("/proxy")
 class ProxyController {
-
-    private val logger = LoggerFactory.getLogger(this::class.java.name)
 
     @GetMapping("/")
     fun getUrlFromProxy(@RequestParam(value = "url") url: String): Any? {
