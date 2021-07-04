d2513cafc12d756bf678da45454e4dbb6d3601b5
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2021-07-04 13:39:28 +0200

    Fix

--- src/main/kotlin/com/dash/entity/ImportData.kt
+++ src/main/kotlin/com/dash/entity/ImportData.kt
@@ -1,7 +1,7 @@
 package com.dash.entity
 
 data class ImportData(
-    val widgets: List<Widget>,
+    val widgets: List<Widget> = listOf(),
 
-    val tabs: List<Tab>
+    val tabs: List<Tab> = listOf()
 )
