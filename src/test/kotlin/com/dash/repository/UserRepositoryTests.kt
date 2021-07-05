4534921bd968c9a31f0b6eae6903477ec85145c8
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2021-07-05 16:22:07 +0200

    FT Ajout de tests

--- src/test/kotlin/com/dash/repository/UserRepositoryTests.kt
+++ src/test/kotlin/com/dash/repository/UserRepositoryTests.kt
@@ -1,5 +1,7 @@
 package com.dash.repository
 
+import com.dash.entity.Role
+import com.dash.entity.User
 import com.dash.enums.RoleEnum
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions.*
@@ -16,12 +18,27 @@ class UserRepositoryTests {
     @Autowired
     private lateinit var userRepository: UserRepository
 
+    @Autowired
+    private lateinit var roleRepository: RoleRepository
+
     @Test
     fun testGetUsers() {
         val listUsers = userRepository.findAll()
         assertThat(listUsers).hasSize(2)
         assertThat(listUsers[0].role.name).isEqualTo(RoleEnum.ROLE_USER)
         assertThat(listUsers[1].role.name).isEqualTo(RoleEnum.ROLE_ADMIN)
+    }
+
+    @Test
+    fun testAddUser() {
+        val roleUser = roleRepository.getOne(1)
+        val newUser = User(id= 0, email= "test@email.com", username = "testusername", password = "testpassword", role = roleUser)
+
+        val insertedUser = userRepository.save(newUser)
+        assertNotNull(insertedUser.id)
+        val listUsers = userRepository.findAll()
+        assertThat(listUsers).hasSize(3)
+        userRepository.delete(insertedUser)
     }
 
     @Test
