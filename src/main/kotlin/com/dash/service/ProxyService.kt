7ab1a046255c04d7b2c58f7b47471b2f3a673133
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2022-03-13 19:40:25 +0100

    fix

--- src/main/kotlin/com/dash/service/ProxyService.kt
+++ src/main/kotlin/com/dash/service/ProxyService.kt
@@ -2,7 +2,6 @@ package com.dash.service
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
-import org.springframework.web.client.RestClientException
 import org.springframework.web.client.RestTemplate
 
 @Service
@@ -11,9 +10,7 @@ class ProxyService {
     @Autowired
     private lateinit var restTemplate: RestTemplate
 
-    @Throws(RestClientException::class)
     fun getDataFromProxy(url: String): String? = restTemplate.getForObject(url, String::class.java)
 
-    @Throws(RestClientException::class)
     fun postDataFromProxy(url: String, data: Any): String? = restTemplate.postForObject(url, data, String::class.java)
 }
