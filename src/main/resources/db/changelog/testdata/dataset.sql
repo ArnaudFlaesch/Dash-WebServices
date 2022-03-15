7feec2f19c91df2cfcf3df0c12120d9ffa6363ae
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2022-03-15 17:32:31 +0100

    remove strava widget

--- src/main/resources/db/changelog/testdata/dataset.sql
+++ src/main/resources/db/changelog/testdata/dataset.sql
@@ -10,5 +10,4 @@ INSERT INTO tab(label, tab_order) VALUES('Steam', 5);
 INSERT INTO widget(type, data, widget_order, tab_id) VALUES(1, '{"city": "Nice"}', 1, (SELECT id FROM tab WHERE label = 'Météo'));
 INSERT INTO widget(type, data, widget_order, tab_id) VALUES(2, '{"url": "https://www.jeuxvideo.com/rss/rss-pc.xml"}', 1, (SELECT id FROM tab WHERE label = 'Flux RSS'));
 INSERT INTO widget(type, data, widget_order, tab_id) VALUES(3, '{"calendarUrls": []}', 1, (SELECT id FROM tab WHERE label = 'Agenda'));
-INSERT INTO widget(type, data, widget_order, tab_id) VALUES(4, '{}', 1, (SELECT id FROM tab WHERE label = 'Strava'));
 INSERT INTO widget(type, data, widget_order, tab_id) VALUES(5, '{}', 1, (SELECT id FROM tab WHERE label = 'Steam'));
