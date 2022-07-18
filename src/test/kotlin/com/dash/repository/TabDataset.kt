bc95b26e873161337982622bff0ccbb76627b077
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2022-07-18 10:12:36 +0200

    Lint 2

--- src/test/kotlin/com/dash/repository/TabDataset.kt
+++ src/test/kotlin/com/dash/repository/TabDataset.kt
@@ -10,10 +10,10 @@ import org.springframework.test.context.jdbc.SqlGroup
     Sql(
         statements = [
             "DELETE FROM widget;" +
-            "DELETE FROM tab;" +
-            "INSERT INTO tab (id, label, tab_order) VALUES ('10', 'News', 1)"
+                "DELETE FROM tab;" +
+                "INSERT INTO tab (id, label, tab_order) VALUES ('10', 'News', 1)"
         ],
-            executionPhase = BEFORE_TEST_METHOD
+        executionPhase = BEFORE_TEST_METHOD
     )
 )
 annotation class TabDataset
