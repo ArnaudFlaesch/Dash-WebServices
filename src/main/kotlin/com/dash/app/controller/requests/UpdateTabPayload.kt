0d3db5b0d31adf5cc2f8281264f7aaac59ed25d7
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2022-10-17 23:45:59 +0200

    lint

--- src/main/kotlin/com/dash/app/controller/requests/UpdateTabPayload.kt
+++ src/main/kotlin/com/dash/app/controller/requests/UpdateTabPayload.kt
@@ -1,5 +1,7 @@
 package com.dash.app.controller.requests
 
 data class UpdateTabPayload(
-    val id: Int, val label: String, val tabOrder: Int
+    val id: Int,
+    val label: String,
+    val tabOrder: Int
 )
