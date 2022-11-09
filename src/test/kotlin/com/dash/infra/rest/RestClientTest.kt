6f5fbd547f58fe965f47ecd32f34d36c9480c4f5
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2022-11-09 14:41:27 +0100

    add tests

--- src/test/kotlin/com/dash/infra/rest/RestClientTest.kt
+++ src/test/kotlin/com/dash/infra/rest/RestClientTest.kt
@@ -1,19 +1,29 @@
 package com.dash.infra.rest
 
 import com.common.utils.AbstractIT
+import com.dash.app.controller.ErrorHandler
 import org.junit.jupiter.api.*
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.extension.ExtendWith
-import org.mockito.Mockito
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.Arguments.arguments
+import org.junit.jupiter.params.provider.MethodSource
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
+import org.springframework.http.MediaType
 import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.test.web.client.ExpectedCount
+import org.springframework.test.web.client.MockRestServiceServer
+import org.springframework.test.web.client.match.MockRestRequestMatchers.method
+import org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo
+import org.springframework.test.web.client.response.MockRestResponseCreators.withStatus
 import org.springframework.web.client.RestTemplate
 import java.net.URI
+import java.util.stream.Stream
 
 @SpringBootTest
 @ExtendWith(SpringExtension::class)
@@ -21,22 +31,75 @@ import java.net.URI
 class RestClientTest : AbstractIT() {
 
     @Autowired
-    private lateinit var proxyService: RestClient
+    private lateinit var restClient: RestClient
 
-    @MockBean
+    @Autowired
     private lateinit var restTemplate: RestTemplate
 
+    private lateinit var mockServer: MockRestServiceServer
+
     val testUrl = "http://url.com"
+
+    @BeforeAll
+    fun setup() {
+        mockServer = MockRestServiceServer.createServer(restTemplate)
+    }
+
+    @BeforeEach
+    fun resetMockServer() {
+        mockServer.reset()
+    }
 
     @Test
     fun testGetRequest() {
         val response = "response"
 
-        Mockito.`when`(restTemplate.exchange(URI.create(testUrl), HttpMethod.GET, null, String::class.java)).thenReturn(
-            ResponseEntity(response, HttpStatus.OK)
-        )
-
-        val actualResponse = proxyService.getDataFromProxy(testUrl, String::class)
+        mockServer.expect(ExpectedCount.once(), requestTo(URI(testUrl)))
+            .andExpect(method(HttpMethod.GET))
+            .andRespond(
+                withStatus(HttpStatus.OK)
+                    .contentType(MediaType.APPLICATION_JSON).body(response)
+            )
+        val actualResponse = restClient.getDataFromProxy(testUrl, String::class)
         assertEquals(response, actualResponse)
+        mockServer.verify()
+    }
+
+    @ParameterizedTest
+    @MethodSource("requestErrorsParams")
+    fun testGetRequestErrors(statusCode: HttpStatus, exceptionClass: Class<Exception>) {
+        mockServer.expect(ExpectedCount.once(), requestTo(URI(testUrl)))
+            .andExpect(method(HttpMethod.GET))
+            .andRespond(
+                withStatus(statusCode)
+                    .contentType(MediaType.APPLICATION_JSON)
+            )
+        assertThrows(exceptionClass) {
+            restClient.getDataFromProxy(testUrl, String::class)
+        }
+        mockServer.verify()
+    }
+
+    @ParameterizedTest
+    @MethodSource("requestErrorsParams")
+    fun testPostRequestErrors(statusCode: HttpStatus, exceptionClass: Class<Exception>) {
+        mockServer.expect(ExpectedCount.once(), requestTo(URI(testUrl)))
+            .andExpect(method(HttpMethod.POST))
+            .andRespond(
+                withStatus(statusCode)
+                    .contentType(MediaType.APPLICATION_JSON)
+            )
+        assertThrows(exceptionClass) {
+            restClient.postDataFromProxy(testUrl, "{}", String::class)
+        }
+        mockServer.verify()
+    }
+
+    fun requestErrorsParams(): Stream<Arguments> {
+        return Stream.of(
+            arguments(HttpStatus.BAD_REQUEST, ErrorHandler.BadRequestException::class.java),
+            arguments(HttpStatus.NOT_FOUND, ErrorHandler.NotFoundException::class.java),
+            arguments(HttpStatus.INTERNAL_SERVER_ERROR, ErrorHandler.InternalServerErrorException::class.java)
+        )
     }
 }
