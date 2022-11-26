04485c4efe3d711690eb81d41883dd48e1a5785c
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2022-11-26 14:03:31 +0100

    test

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ plugins {
     val kotlinVersion = "1.7.21"
     val springBootVersion = "2.7.6"
     val springDependencyManagementVersion = "1.1.0"
-    val koverVersion = "0.6.1"
+    val koverVersion = "0.5.1"
     val codacyPluginVersion = "0.1.0"
     val detektVersion = "1.19.0"
     val springDocGradlePluginVersion = "1.5.0"
@@ -47,7 +47,7 @@ plugins {
 }
 
 group = "com.dash"
-version = "0.4.0"
+version = "1.0.0"
 java.sourceCompatibility = JavaVersion.VERSION_16
 
 repositories {
