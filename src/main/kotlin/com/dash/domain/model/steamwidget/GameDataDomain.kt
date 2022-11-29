3aaa868c693d4e54b46da54830c793c09a1e8921
Arnaud Flaesch <ArnaudFlaesch@users.noreply.github.com>
2022-11-29 22:34:17 +0100

    FT # Add tests (#314)

--- src/main/kotlin/com/dash/domain/model/steamwidget/GameDataDomain.kt
+++ src/main/kotlin/com/dash/domain/model/steamwidget/GameDataDomain.kt
@@ -1,7 +1,5 @@
 package com.dash.domain.model.steamwidget
 
-import com.dash.infra.apimodel.steam.GameInfoApi
-
 data class GameDataDomain(
     val gameCount: Int,
     val games: List<GameInfoDomain>
@@ -18,18 +16,4 @@ data class GameInfoDomain(
     val playtimeWindowsForever: Int,
     val playtimeMacForever: Int,
     val playtimeLinuxForever: Int
-) {
-    fun gameDataApiToDomain(game: GameInfoApi): GameInfoDomain =
-        GameInfoDomain(
-            appid = game.appid,
-            name = game.name,
-            imgIconUrl = game.imgIconUrl,
-            imgLogoUrl = game.imgLogoUrl,
-            hasCommunityVisibleStats = game.hasCommunityVisibleStats,
-            playtime2weeks = game.playtime2weeks,
-            playtimeForever = game.playtimeForever,
-            playtimeWindowsForever = game.playtimeWindowsForever,
-            playtimeMacForever = game.playtimeMacForever,
-            playtimeLinuxForever = game.playtimeLinuxForever
-        )
-}
+)
---
+++ src/test/kotlin/com/dash/infra/repository/TwitterRepositoryTests.kt
@@ -0,0 +1,38 @@
+package com.dash.infra.repository
+
+import com.common.infra.repository.UserRepository
+import com.common.utils.AbstractIT
+import com.dash.infra.entity.twitterwidget.FollowedUserEntity
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit.jupiter.SpringExtension
+
+@SpringBootTest
+@ExtendWith(SpringExtension::class)
+class TwitterRepositoryTests : AbstractIT() {
+
+    @Autowired
+    private lateinit var twitterWidgetRepository: TwitterWidgetRepository
+
+    @Autowired
+    private lateinit var userRepository: UserRepository
+
+    @Test
+    fun testInsertFollowedUsers() {
+        val userId = 1
+        val followedUser1 = FollowedUserEntity(id = 0, userHandle = "Nono", user = userRepository.getReferenceById(userId))
+        val followedUser2 = FollowedUserEntity(id = 0, userHandle = "Nono2", user = userRepository.getReferenceById(userId))
+        twitterWidgetRepository.save(followedUser1)
+        twitterWidgetRepository.save(followedUser2)
+
+        val followedUsersList = twitterWidgetRepository.searchFollowedUsers("", userId)
+        assertThat(followedUsersList).hasSize(2)
+        assertThat(followedUsersList[0].userHandle).isEqualTo("Nono")
+        assertThat(followedUsersList[0].user.id).isEqualTo(userId)
+        assertThat(followedUsersList[1].userHandle).isEqualTo("Nono2")
+        assertThat(followedUsersList[1].user.id).isEqualTo(userId)
+    }
+}
