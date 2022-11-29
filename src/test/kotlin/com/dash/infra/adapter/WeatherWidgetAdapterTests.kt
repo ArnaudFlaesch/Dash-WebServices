bb646e864f5918a3dec36aec8ee1d7bfaaa1b7d9
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2022-11-29 21:47:24 +0100

    test

---
+++ src/test/kotlin/com/dash/infra/adapter/WeatherWidgetAdapterTests.kt
@@ -0,0 +1,38 @@
+package com.dash.infra.adapter
+
+import com.dash.infra.rest.OpenWeatherApiClient
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.kotlin.given
+
+@ExtendWith(MockitoExtension::class)
+class WeatherWidgetAdapterTests {
+
+    @Mock
+    private lateinit var weatherApiClient: OpenWeatherApiClient
+
+    @InjectMocks
+    private lateinit var weatherWidgetAdapter: WeatherWidgetAdapter
+
+    @Test
+    fun should_return_empty_weather_data() {
+        val city = "Paris"
+        given(weatherApiClient.getWeatherData(city)).willReturn(null)
+        val response = weatherWidgetAdapter.getWeatherData(city)
+        assertEquals(0, response.weather.size)
+        assertEquals("", response.name)
+    }
+
+    @Test
+    fun should_return_empty_forecast_data() {
+        val city = "Paris"
+        given(weatherApiClient.getForecastData(city)).willReturn(null)
+        val response = weatherWidgetAdapter.getForecastData(city)
+        assertEquals(0, response.list.size)
+        assertEquals("", response.city.name)
+    }
+}
