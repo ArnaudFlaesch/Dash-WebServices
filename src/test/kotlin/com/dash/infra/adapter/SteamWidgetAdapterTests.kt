0931975e027542f9f66874324db037bf3cb15908
Arnaud Flaesch <ArnaudFlaesch@users.noreply.github.com>
2022-12-09 13:27:57 +0100

    FT #327 : Fix pagination (#328)
    * FT #327 : Fix pagination

---
+++ src/test/kotlin/com/dash/infra/adapter/SteamWidgetAdapterTests.kt
@@ -0,0 +1,52 @@
+package com.dash.infra.adapter
+
+import com.dash.domain.model.steamwidget.GameDataDomain
+import com.dash.infra.apimodel.steam.GameDataApi
+import com.dash.infra.apimodel.steam.GameInfoApi
+import com.dash.infra.apimodel.steam.GameInfoResponse
+import com.dash.infra.rest.SteamApiClient
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.kotlin.given
+
+@ExtendWith(MockitoExtension::class)
+class SteamWidgetAdapterTests {
+
+    @Mock
+    private lateinit var steamApiClient: SteamApiClient
+
+    @InjectMocks
+    private lateinit var steamWidgetAdapter: SteamWidgetAdapter
+
+    @Test
+    fun should_paginate_results() {
+        val steamUserId = "1337"
+        val gamesListMock = createGameListFromApi(0, 50)
+        val gameInfoResponseMock = GameInfoResponse(GameDataApi(50, gamesListMock))
+        given(steamApiClient.getOwnedGames(steamUserId)).willReturn(gameInfoResponseMock)
+
+        val actualFirstPage = steamWidgetAdapter.getOwnedGames(steamUserId, "", 0)
+        val expectedFirstPage = GameDataDomain(
+            gameCount = 50,
+            games = gamesListMock.subList(0, 25).map(GameInfoApi::toDomain)
+        )
+        assertEquals(expectedFirstPage, actualFirstPage)
+
+        val actualSecondPage = steamWidgetAdapter.getOwnedGames(steamUserId, "", 1)
+        val expectedSecondPage = GameDataDomain(
+            gameCount = 50,
+            games = gamesListMock.subList(25, 50).map(GameInfoApi::toDomain)
+        )
+        assertEquals(expectedSecondPage, actualSecondPage)
+    }
+
+    private fun createGameListFromApi(startIndex: Int, size: Int): List<GameInfoApi> {
+        return (startIndex until size).map { index ->
+            GameInfoApi(appid = index.toString(), name = "Call of Duty $index")
+        }.sortedBy(GameInfoApi::name)
+    }
+}
--- src/test/kotlin/com/dash/infra/adapter/WeatherWidgetAdapterTests.kt
+++ src/test/kotlin/com/dash/infra/adapter/WeatherWidgetAdapterTests.kt
@@ -1,5 +1,6 @@
 package com.dash.infra.adapter
 
+import com.dash.infra.apimodel.openweather.*
 import com.dash.infra.rest.OpenWeatherApiClient
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
@@ -19,12 +20,39 @@ class WeatherWidgetAdapterTests {
     private lateinit var weatherWidgetAdapter: WeatherWidgetAdapter
 
     @Test
+    fun should_return_weather_data() {
+        val city = "Paris"
+
+        val weatherListResponse = listOf(Weather(icon = "sunny"), Weather(icon = "cloudy"))
+        val weatherApiData = OpenWeatherWeatherResponse(name = city, id = 123, weather = weatherListResponse)
+
+        given(weatherApiClient.getWeatherData(city)).willReturn(weatherApiData)
+        val response = weatherWidgetAdapter.getWeatherData(city)
+        assertEquals(2, response.weather.size)
+        assertEquals(city, response.name)
+    }
+
+    @Test
     fun should_return_empty_weather_data() {
         val city = "Paris"
         given(weatherApiClient.getWeatherData(city)).willReturn(null)
         val response = weatherWidgetAdapter.getWeatherData(city)
         assertEquals(0, response.weather.size)
         assertEquals("", response.name)
+    }
+
+    @Test
+    fun should_return_forecast_data() {
+        val city = "Paris"
+
+        val cityResponse = CityResponse(name = city, country = "France")
+        val forecastList = listOf(ForecastResponse(dt = 1), ForecastResponse(dt = 2))
+        val forecastData = OpenWeatherForecastResponse(list = forecastList, city = cityResponse)
+
+        given(weatherApiClient.getForecastData(city)).willReturn(forecastData)
+        val response = weatherWidgetAdapter.getForecastData(city)
+        assertEquals(2, response.list.size)
+        assertEquals(city, response.city.name)
     }
 
     @Test
