6551febae6bcd5cad61229b6b503cfd239319cfb
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2023-01-14 18:42:34 +0100

    Deploy render

---
+++ src/main/kotlin/com/common/app/controller/HealthCheckController.kt
@@ -0,0 +1,19 @@
+package com.common.app.controller
+
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+@CrossOrigin(origins = ["*"], maxAge = 3600)
+@RestController
+@RequestMapping("/healthCheck")
+class HealthCheckController() {
+
+    @GetMapping("/status")
+    fun healthCheck(): ResponseEntity<*> {
+        return ResponseEntity
+            .ok()
+            .contentType(MediaType.TEXT_PLAIN)
+            .body("Application is running.")
+    }
+}
