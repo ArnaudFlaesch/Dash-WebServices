189199b92ccc629a23b8ee21be52dc8d41d299c0
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2023-02-03 21:22:02 +0100

    lint

--- src/main/kotlin/com/dash/infra/entity/workoutwidget/WorkoutExerciseEntity.kt
+++ src/main/kotlin/com/dash/infra/entity/workoutwidget/WorkoutExerciseEntity.kt
@@ -6,52 +6,93 @@ import java.io.Serializable
 
 @NamedNativeQueries(
     NamedNativeQuery(
-        query = "SELECT sum(E.number_of_reps) AS totalNumberOfReps, T.name AS workoutTypeName\n" + "FROM Workout_Exercise E\n" + "RIGHT JOIN Workout_Session S ON E.workout_session_id = S.id\n" + "RIGHT JOIN Workout_Type T ON E.workout_type_id = T.id\n" + "WHERE S.workout_date BETWEEN :dateIntervalStart AND :dateIntervalEnd\n" + "AND S.user_id = :userId\n" + "GROUP BY T.name",
+        query = "SELECT sum(E.number_of_reps) AS totalNumberOfReps, T.name AS workoutTypeName\n" +
+            "FROM Workout_Exercise E\n" +
+            "RIGHT JOIN Workout_Session S ON E.workout_session_id = S.id\n" +
+            "RIGHT JOIN Workout_Type T ON E.workout_type_id = T.id\n" +
+            "WHERE S.workout_date BETWEEN :dateIntervalStart AND :dateIntervalEnd\n" +
+            "AND S.user_id = :userId\n" + "GROUP BY T.name",
         name = "getWorkoutStatsByInterval",
         resultClass = WorkoutStatsByIntervalEntity::class,
         resultSetMapping = "workoutStatsByInterval"
-    ), NamedNativeQuery(
-        query = "SELECT E.workout_type_id AS workoutTypeId, SUM(E.number_of_reps) as totalNumberOfReps,\n" + "T.name as workoutTypeName, EXTRACT(MONTH FROM S.workout_date) AS monthPeriod,\n" + "EXTRACT(YEAR FROM S.workout_date) AS yearPeriod\n" + "FROM Workout_Exercise E\n" + "RIGHT JOIN Workout_Session S ON E.workout_session_id = S.id\n" + "RIGHT JOIN Workout_Type T ON E.workout_type_id = T.id\n" + "WHERE EXTRACT(YEAR FROM S.workout_date) = :year\n" + "AND S.user_id = :userId\n" + "GROUP BY monthPeriod, workoutTypeId, yearPeriod, workoutTypeName\n" + "ORDER BY monthPeriod ASC",
+    ),
+    NamedNativeQuery(
+        query = "SELECT E.workout_type_id AS workoutTypeId, SUM(E.number_of_reps) as totalNumberOfReps,\n" +
+            "T.name as workoutTypeName, EXTRACT(MONTH FROM S.workout_date) AS monthPeriod,\n" +
+            "EXTRACT(YEAR FROM S.workout_date) AS yearPeriod\n" +
+            "FROM Workout_Exercise E\n" +
+            "RIGHT JOIN Workout_Session S ON E.workout_session_id = S.id\n" +
+            "RIGHT JOIN Workout_Type T ON E.workout_type_id = T.id\n" +
+            "WHERE EXTRACT(YEAR FROM S.workout_date) = :year\n" +
+            "AND S.user_id = :userId\n" +
+            "GROUP BY monthPeriod, workoutTypeId, yearPeriod, workoutTypeName\n" +
+            "ORDER BY monthPeriod ASC",
         name = "getWorkoutStatsByYear",
         resultClass = WorkoutStatsByYearEntity::class,
         resultSetMapping = "workoutStatsByYear"
     )
 )
-
 @SqlResultSetMappings(
     SqlResultSetMapping(
-        name = "workoutStatsByYear", classes = [ConstructorResult(
-            targetClass = WorkoutStatsByYearEntity::class,
-            columns = [ColumnResult(name = "workoutTypeId"), ColumnResult(name = "totalNumberOfReps"), ColumnResult(name = "workoutTypeName"), ColumnResult(name = "monthPeriod"), ColumnResult(
-                name = "yearPeriod"
-            )]
-        )]
-    ), SqlResultSetMapping(
-        name = "workoutStatsByInterval", classes = [ConstructorResult(
-            targetClass = WorkoutStatsByIntervalEntity::class, columns = [ColumnResult(name = "totalNumberOfReps"), ColumnResult(name = "workoutTypeName")]
-        )]
+        name = "workoutStatsByYear",
+        classes = [
+            ConstructorResult(
+                targetClass = WorkoutStatsByYearEntity::class,
+                columns = [
+                    ColumnResult(name = "workoutTypeId"),
+                    ColumnResult(name = "totalNumberOfReps"),
+                    ColumnResult(name = "workoutTypeName"),
+                    ColumnResult(name = "monthPeriod"),
+                    ColumnResult(
+                        name = "yearPeriod"
+                    )
+                ]
+            )
+        ]
+    ),
+    SqlResultSetMapping(
+        name = "workoutStatsByInterval",
+        classes = [
+            ConstructorResult(
+                targetClass = WorkoutStatsByIntervalEntity::class,
+                columns = [
+                    ColumnResult(name = "totalNumberOfReps"),
+                    ColumnResult(name = "workoutTypeName")
+                ]
+            )
+        ]
     )
 )
 @Entity
 @Table(name = "workout_exercise")
 data class WorkoutExerciseEntity(
-    @EmbeddedId var workoutExerciseId: WorkoutExerciseEntityId, var numberOfReps: Int = 0,
+    @EmbeddedId
+    var workoutExerciseId: WorkoutExerciseEntityId,
+    var numberOfReps: Int = 0,
 
-    @ManyToOne @JoinColumn(name = "workout_session_id", insertable = false, updatable = false) var workoutSession: WorkoutSessionEntity? = null,
+    @ManyToOne
+    @JoinColumn(name = "workout_session_id", insertable = false, updatable = false)
+    var workoutSession: WorkoutSessionEntity? = null,
 
-    @ManyToOne @JoinColumn(name = "workout_type_id", insertable = false, updatable = false) var workoutType: WorkoutTypeEntity? = null
+    @ManyToOne
+    @JoinColumn(name = "workout_type_id", insertable = false, updatable = false)
+    var workoutType: WorkoutTypeEntity? = null
 ) {
     fun toDomain(): WorkoutExerciseDomain = WorkoutExerciseDomain(
-        workoutSessionId = this.workoutExerciseId.workoutSessionId, workoutTypeId = this.workoutExerciseId.workoutTypeId, numberOfReps = this.numberOfReps
+        workoutSessionId = this.workoutExerciseId.workoutSessionId,
+        workoutTypeId = this.workoutExerciseId.workoutTypeId,
+        numberOfReps = this.numberOfReps
     )
 }
 
 @Embeddable
 data class WorkoutExerciseEntityId(
 
-    @Column(name = "workout_session_id") val workoutSessionId: Int,
+    @Column(name = "workout_session_id")
+    val workoutSessionId: Int,
 
-    @Column(name = "workout_type_id") val workoutTypeId: Int
+    @Column(name = "workout_type_id")
+    val workoutTypeId: Int
 ) : Serializable {
     companion object {
         private const val serialVersionUID: Long = 1L
