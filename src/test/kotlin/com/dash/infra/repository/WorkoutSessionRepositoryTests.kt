84cd283add9f2050302a72073de1f79e3456c50e
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2023-04-07 22:05:14 +0200

    add tests

---
+++ src/test/kotlin/com/dash/infra/repository/WorkoutSessionRepositoryTests.kt
@@ -0,0 +1,41 @@
+package com.dash.infra.repository
+
+import com.common.infra.repository.UserRepository
+import com.common.utils.AbstractIT
+import com.dash.infra.entity.workoutwidget.WorkoutSessionEntity
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import java.time.LocalDate
+
+@SpringBootTest
+class WorkoutSessionRepositoryTests : AbstractIT() {
+
+    @Autowired
+    private lateinit var workoutSessionRepository: WorkoutSessionRepository
+
+    @Autowired
+    private lateinit var userRepository: UserRepository
+
+    @Test
+    fun testInsertWorkoutSessions() {
+        val authenticatedUser = userRepository.getReferenceById(1)
+        val workoutSessions = listOf(
+            WorkoutSessionEntity(0, LocalDate.of(2023, 1, 1), authenticatedUser), WorkoutSessionEntity(0, LocalDate.of(2023, 1, 5), authenticatedUser)
+        )
+
+        workoutSessionRepository.saveAll(workoutSessions)
+
+        val listWorkoutSessions = workoutSessionRepository.findByUserIdAndWorkoutDateBetweenOrderByWorkoutDateAsc(
+            authenticatedUser.id, LocalDate.of(2023, 1, 1), LocalDate.now()
+        )
+        assertThat(listWorkoutSessions).hasSize(2)
+
+        assertEquals(LocalDate.of(2023, 1, 1), listWorkoutSessions[0].workoutDate)
+        assertEquals(authenticatedUser.id, listWorkoutSessions[0].user.id)
+        assertEquals(LocalDate.of(2023, 1, 5), listWorkoutSessions[1].workoutDate)
+        assertEquals(authenticatedUser.id, listWorkoutSessions[1].user.id)
+    }
+}
