51cd8d72fa2b6c5cdaf1dfbf9acf2267b1e32ecf
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2023-04-07 21:19:19 +0200

    test

--- src/test/kotlin/com/dash/infra/repository/MiniWidgetRepositoryTests.kt
+++ src/test/kotlin/com/dash/infra/repository/MiniWidgetRepositoryTests.kt
@@ -4,6 +4,7 @@ import com.common.infra.repository.UserRepository
 import com.common.utils.AbstractIT
 import com.dash.infra.entity.MiniWidgetEntity
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -20,12 +21,14 @@ class MiniWidgetRepositoryTests : AbstractIT() {
     @Test
     fun testInsertWidgets() {
         val authenticatedUser = userRepository.getReferenceById(1)
-        val w1 = MiniWidgetEntity(id = 0, type = 1, data = "{}", user = authenticatedUser)
-        val w2 = MiniWidgetEntity(id = 0, type = 1, data = "{}", user = authenticatedUser)
+        val w1 = MiniWidgetEntity(0, 1, "{}", authenticatedUser)
+        val w2 = MiniWidgetEntity(0, 1, "{}", authenticatedUser)
         miniWidgetRepository.save(w1)
         miniWidgetRepository.save(w2)
 
         val listWidgets = miniWidgetRepository.findByUserId(authenticatedUser.id)
         assertThat(listWidgets).hasSize(2)
+        assertEquals(1, listWidgets[0].type)
+        assertEquals(1, listWidgets[1].type)
     }
 }
