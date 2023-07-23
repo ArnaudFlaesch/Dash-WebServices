6272dac74fc9c5d3ec1096a7ad166c1fbd4d6eb3
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2023-07-23 21:14:41 +0200

    FT #823 : Fix permission

--- src/main/kotlin/com/common/app/security/annotations/IncidentWidgetSecurityService.kt
+++ src/main/kotlin/com/common/app/security/annotations/IncidentWidgetSecurityService.kt
@@ -1,22 +1,22 @@
 package com.common.app.security.annotations
 
 import com.common.domain.service.UserService
-import com.dash.domain.model.incidentWidget.IncidentDomain
-import com.dash.infra.adapter.IncidentWidgetAdapter
+import com.dash.domain.model.WidgetDomain
 import com.dash.infra.adapter.WidgetPersistenceAdapter
+import com.dash.infra.repository.IncidentWidgetRepository
 import org.springframework.stereotype.Component
 
 @Component
 class IncidentWidgetSecurityService(
     val userService: UserService,
     val widgetPersistenceAdapter: WidgetPersistenceAdapter,
-    val incidentWidgetAdapter: IncidentWidgetAdapter
+    val incidentWidgetRepository: IncidentWidgetRepository
 ) {
 
     fun doesIncidentBelongToAuthenticatedUser(incidentId: Int): Boolean {
         val authUser = userService.getCurrentAuthenticatedUser()
+        val incidentConfig = incidentWidgetRepository.getReferenceById(incidentId)
         val userWidgets = widgetPersistenceAdapter.getUserWidgets(authUser.id)
-        val userIncidentWidgets = userWidgets.map { widget -> incidentWidgetAdapter.getIncidentConfigForWidget(widget.id) }
-        return userIncidentWidgets.map(IncidentDomain::id).contains(incidentId)
+        return userWidgets.map(WidgetDomain::id).contains(incidentConfig.widget.id)
     }
 }
