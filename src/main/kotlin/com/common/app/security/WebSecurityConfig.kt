b1e308a91aa60af5070767a3ac8d2322f5054be9
Arnaud Flaesch <ArnaudFlaesch@users.noreply.github.com>
2023-11-05 19:31:54 +0100

    Remove deprecated functions (#461)
    * deprecated
    
    * test
    
    * lint

--- src/main/kotlin/com/common/app/security/WebSecurityConfig.kt
+++ src/main/kotlin/com/common/app/security/WebSecurityConfig.kt
@@ -3,6 +3,7 @@ package com.common.app.security
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
@@ -14,6 +15,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import java.util.*
 
 @Configuration
 @EnableWebSecurity
@@ -43,14 +48,27 @@ class WebSecurityConfig(
         val authenticationManager = authenticationManagerBuilder.build()
 
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter::class.java)
-            .cors().and().csrf().disable()
-            .authorizeHttpRequests().requestMatchers("/auth/**", "/v3/api-docs/**", "/error")
-            .permitAll()
-            .anyRequest().authenticated().and()
+            .cors(Customizer.withDefaults())
+            .csrf { csrf -> csrf.disable() }
+            .authorizeHttpRequests { authorizeRequests ->
+                authorizeRequests.requestMatchers("/auth/**", "/v3/api-docs", "/error").permitAll()
+                    .anyRequest().authenticated()
+            }
             .authenticationManager(authenticationManager)
-            .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
-            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-
+            .exceptionHandling { auth -> auth.authenticationEntryPoint(unauthorizedHandler) }
+            .sessionManagement { sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
         return http.build()
+    }
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val config = CorsConfiguration()
+        config.allowedOriginPatterns = listOf("*")
+        config.setAllowedMethods(listOf("GET", "POST", "OPTIONS", "DELETE", "PUT", "PATCH"))
+        config.allowCredentials = true
+        config.allowedHeaders = listOf("Authorization", "Cache-Control", "Content-Type")
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", config)
+        return source
     }
 }
