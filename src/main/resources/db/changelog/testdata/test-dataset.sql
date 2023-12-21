494e2ad4542ec633154fba53914feb546177fb99
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2023-12-21 10:39:02 +0100

    fix

--- src/main/resources/db/changelog/testdata/test-dataset.sql
+++ src/main/resources/db/changelog/testdata/test-dataset.sql
@@ -1,4 +1,4 @@
-INSERT INTO public.users(username, password, email, role_id) VALUES('usertest', '$2a$10$EovHPEXDpFNDFek8ERvTdufGypWTDJF6OoFGHYU.e1XOyqiuzH2fe', 'user@email.com', 1);
-INSERT INTO public.users(username, password, email, role_id) VALUES('admintest', '$2a$10$EovHPEXDpFNDFek8ERvTduFaJxwsWI1KCIFER/B3TvSXEebbCLxFG', 'admin@email.com', 2);
+INSERT INTO public.users(username, password, email, role_id) VALUES ('usertest', '$2a$10$EovHPEXDpFNDFek8ERvTdufGypWTDJF6OoFGHYU.e1XOyqiuzH2fe', 'user@email.com', 1);
+INSERT INTO public.users(username, password, email, role_id) VALUES ('admintest', '$2a$10$EovHPEXDpFNDFek8ERvTduFaJxwsWI1KCIFER/B3TvSXEebbCLxFG', 'admin@email.com', 2);
 
-INSERT INTO public.tab(label, tab_order, user_id) VALUES('Home', 1, (SELECT id from public.users WHERE username = 'admintest'));
+INSERT INTO public.tab(label, tab_order, user_id) VALUES ('Home', 1, (SELECT id from public.users WHERE username = 'admintest'));
