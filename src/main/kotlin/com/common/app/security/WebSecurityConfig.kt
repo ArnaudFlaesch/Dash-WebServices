298acef3d53e8e5403b798d706b0255bbd8f283a
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2024-03-12 21:47:02 +0100

    fix

--- src/main/kotlin/com/common/app/security/WebSecurityConfig.kt
+++ src/main/kotlin/com/common/app/security/WebSecurityConfig.kt
@@ -3,6 +3,7 @@ package com.common.app.security
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
