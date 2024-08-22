74df4514a6a3edfe4e657d9e10e945ae79eedfe2
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2024-08-22 16:53:42 +0200

    revert

--- src/main/resources/db/changelog/1.0/configdata.sql
+++ src/main/resources/db/changelog/1.0/configdata.sql
@@ -1,14 +1,14 @@
-INSERT INTO PUBLIC.widget_type (description, config) VALUES ('WEATHER', '["weather_api_key", "city"]');
-INSERT INTO PUBLIC.widget_type (description, config) VALUES ('RSS', '["url"]');
-INSERT INTO PUBLIC.widget_type (description, config) VALUES ('CALENDAR', '[{"calendars": "list"}]');
-INSERT INTO PUBLIC.widget_type (description, config) VALUES ('STRAVA', '["clientId", "clientSecret"]');
-INSERT INTO PUBLIC.widget_type (description, config) VALUES ('STEAM', '["steamUserId"]');
-INSERT INTO PUBLIC.widget_type (description, config) VALUES ('WORKOUT', '{}');
-INSERT INTO PUBLIC.widget_type (description, config) VALUES ('AIRPARIF', '["airParifApiKey", "communeInseeCode"]');
-INSERT INTO PUBLIC.widget_type (description, config) VALUES ('TWITTER', '["twitterHandle"]');
-INSERT INTO PUBLIC.widget_type (description, config) VALUES ('ECOWATT', '[]');
+INSERT INTO public.widget_type (description, config) VALUES ('WEATHER', '["weather_api_key", "city"]');
+INSERT INTO public.widget_type (description, config) VALUES ('RSS', '["url"]');
+INSERT INTO public.widget_type (description, config) VALUES ('CALENDAR', '[{"calendars": "list"}]');
+INSERT INTO public.widget_type (description, config) VALUES ('STRAVA', '["clientId", "clientSecret"]');
+INSERT INTO public.widget_type (description, config) VALUES ('STEAM', '["steamUserId"]');
+INSERT INTO public.widget_type (description, config) VALUES ('WORKOUT', '{}');
+INSERT INTO public.widget_type (description, config) VALUES ('AIRPARIF', '["airParifApiKey", "communeInseeCode"]');
+INSERT INTO public.widget_type (description, config) VALUES ('TWITTER', '["twitterHandle"]');
+INSERT INTO public.widget_type (description, config) VALUES ('ECOWATT', '[]');
 
-INSERT INTO PUBLIC.mini_widget_type (description, config) VALUES ('WEATHER', '["weather_api_key", "city"]');
+INSERT INTO public.mini_widget_type (description, config) VALUES ('WEATHER', '["weather_api_key", "city"]');
 
-INSERT INTO PUBLIC.roles (name) VALUES('ROLE_USER');
-INSERT INTO PUBLIC.roles (name) VALUES('ROLE_ADMIN');
+INSERT INTO public.roles (name) VALUES('ROLE_USER');
+INSERT INTO public.roles (name) VALUES('ROLE_ADMIN');
