e3cbec2a775a1348a51e43b1d330e8f586fc12da
Arnaud Flaesch <ArnaudFlaesch@users.noreply.github.com>
2024-11-23 20:51:11 +0100

    Revert spring boot plugin update (#724)

--- src/test/kotlin/com/dash/domain/service/AirParifWidgetServiceTest.kt
+++ src/test/kotlin/com/dash/domain/service/AirParifWidgetServiceTest.kt
@@ -8,18 +8,18 @@ import org.mockito.Mockito
 import org.mockito.kotlin.any
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.core.ParameterizedTypeReference
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.test.context.bean.override.mockito.MockitoBean
 import org.springframework.web.client.RestTemplate
 import java.net.URI
 
 @SpringBootTest
 class AirParifWidgetServiceTest {
-    @MockitoBean
+    @MockBean
     private lateinit var restTemplate: RestTemplate
 
     @Autowired
