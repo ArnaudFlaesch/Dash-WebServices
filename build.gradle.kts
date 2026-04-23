b9185f71433d949d835fc5c631c8267d3b72148b
Arnaud Flaesch <arnaudflaesch.dev@gmail.com>
2026-04-24 00:02:40 +0200

    Update springCloudGcpVersion to v8

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ val mockitoKotlinVersion = "6.3.0"
 val junitPlatformLauncherVersion = "6.0.1"
 val hibernateTypesVersion = "2.21.1"
 
-val springCloudGcpVersion = "6.5.6"
+val springCloudGcpVersion = "8.0.2"
 val springCloudVersion = "2025.1.1"
 
 plugins {
